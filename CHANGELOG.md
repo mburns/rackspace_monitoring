c320fc3e23c3b5b808a4a8923945f684e3716c39
Michael Burns <michael.burns@rackspace.com>
2015-05-03 04:37:41 -0700

    version 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 0.2.0
+
+Include LWRP resources for dynamicly adding and removing Agent, Checks and Plugins.
+Initial testing coverage with Serverspec and Test-Kitchen
+
 # 0.1.0
 
 Initial release of rackspace_monitoring
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ele-dev@lists.rackspace.com'
 license 'Apache 2.0'
 description 'Installs/Configures rackspace monitoring'
 long_description 'Installs/Configures rackspace monitoring'
-version '0.1.0'
+version '0.2.0'
 
 %w(ubuntu debian redhat centos amazon oracle).each do |os|
   supports os
